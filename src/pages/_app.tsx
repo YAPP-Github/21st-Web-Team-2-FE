6439272b7cdf0356b83851986a020d497d5a37b3
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-11-24 19:10:29 +0900

    [chore] 잘못 설정한 react-query 대신 @tanstack/react-query 로 대체 및 react-query devtools 설치 및 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
+
+import queryClient from '../configs/queryClient';
 
 function MyApp({ Component, pageProps }: AppProps) {
   /*
@@ -11,7 +15,10 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <RecoilRoot>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </RecoilRoot>
   );
 }
