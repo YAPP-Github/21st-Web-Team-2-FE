1293788fa0cb57cdf2a979a4461f484355e42ac6
kimseunggyu <dev.seunggyu@gmail.com>
2022-11-24 16:03:29 +0900

    [style] 모든 파일 prettier 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   /*
