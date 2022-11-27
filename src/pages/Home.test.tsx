9405365318285e1cccce8d04cc5865e201d7f395
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-11-28 00:11:56 +0900

    [fix] 🐛 nextjs pages 폴더에 test 코드(파일)가 있는 경우 build 에러 수정 - pages 의 파일명을 변경함으로서 수정

--- src/pages/Home.test.tsx
+++ src/pages/Home.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 
-import Home from './index';
+import Home from './index.page';
 
 describe('Home', () => {
   it('renders a headig', () => {
