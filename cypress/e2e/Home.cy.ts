a19b1b188f97d67e8b6d002e6ca4bbb51b81bf4b
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-11-28 00:58:01 +0900

    [chore] cypress testing-library 적용 및 설치

--- cypress/e2e/Home.cy.ts
+++ cypress/e2e/Home.cy.ts
@@ -2,12 +2,15 @@
 
 describe('Home Page', () => {
   it('렌더링 테스트', () => {
-    // Start from the index page
     cy.visit('/');
 
-    // Find a link with an href attribute containing "about" and click it
-
-    // The new page should contain an h1 with "About page"
     cy.get('h2').contains(/Hello Web2/i);
+  });
+
+  it('렌더링 테스트 - React-testing-library', () => {
+    cy.visit('/');
+
+    // cy.findByRole('heading').contains(/Hello Web2/i);
+    cy.findByRole('heading').contains(/Hello Web2/i);
   });
 });
