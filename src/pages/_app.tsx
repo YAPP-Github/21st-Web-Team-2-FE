e1db2d7840fec9607833062a22f4f426e087766f
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2022-11-27 20:31:37 +0900

    프로젝트 초기 설정 (#3)
    * [chore] nextjs typescript install
    * [chore] github template 추가
    - 이슈
    - 풀리퀘
    * [chore] prettier 설정
    * [chore] emotion-styled 추가
    * [chore] recoil 설치 및 provider 설정
    * [chore] react-query 및 axios 설치
    * [chore] eslint 설치 및 적용
    - @typescript-eslint/eslint-plugin (타입스크립트 eslint)
    - eslint-config-next (next eslint)
    - prettier
    - typescript
    - eslint-config-prettier, eslint-plugin-prettier (prettier 적용)
    * [chore] prettier-plugin-sort-imports 설정
    * [style] 모든 파일 prettier 적용
    * [chore] gitignore 추가
    - window, mac, nextjs, webstorm
    * [chore] prettier 를 위한 .vscode 수정 및 PR_TEMPLATE 수정
    * [refactor] src 폴더로 이동
    * [chore] 잘못 설정한 react-query 대신 @tanstack/react-query 로 대체 및 react-query devtools 설치 및 설정
    * [refactor] 불필요한 코드 제거
    - 사용하지 않은 정적 파일 제거
    - lint 예제 코드 제거

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import type { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
+
+import queryClient from '../configs/queryClient';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </RecoilRoot>
+  );
+}
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return <h2>Hello Web2</h2>;
+};
+
+export default Home;
