28c7db3099250671c86f745a5182a65e47a8c416
hwookim <gusdn9754@gmail.com>
2022-11-30 11:31:42 +0900

    chore: NextImage가 storybook에서 작동하도록 설정
    - optimized 옵션 비활성화

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,18 @@
+import * as NextImage from 'next/image';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
