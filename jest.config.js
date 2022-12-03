efc3ccbb2246114b39707d2a985b562e60b83387
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2022-12-03 20:20:54 +0900

    [feature/004] Jest, React-testing-library, cypress 설정 (#10)
    * [chore] jest RTL 설치 및 설정
    
    * [chore] react-testing-library eslint 설정
    
    * [chore] jest-dom plugin 설치 및 설정
    - RTL 을 갖고 dom 테스트할 때, jest 매처대신 올바른 jest-dom 매처 사용하도록 설정
    
    * [chore] @testing-library/user-event 설치
    
    * [chore] cypress 설치 및 설정
    - cypress 용 gitignore 추가
    - eslint 설정
    - cypress default url 설정
    - tsconfig 설정
    
    * [fix] 🐛 cypress jest-dom 충돌 해결
    - tsconfig 에서 cypress, cypress.config.ts 추가
    
    * [fix] 🐛 nextjs pages 폴더에 test 코드(파일)가 있는 경우 build 에러 수정
    - pages 의 파일명을 변경함으로서 수정
    
    * [chore] cypress testing-library 적용 및 설치
    
    * [chore] cypress test script 변경
    - cypress 실행시킬시, dev 환경이 아닌 prod 환경으로 실행시켜야함
    (ISR 방식 도입시 테스트를 위함)
    
    * [chore] cypress e2e script 수정
    - e2e 수행시, build 시키는 명령어 추가

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/pages/(.*)$': '<rootDir>/src/pages/$1',
+  },
+  testEnvironment: 'jest-environment-jsdom',
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
