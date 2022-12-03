6ab2813e1c01024613ec80f09c4a35c7c6cee699
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-12-04 03:51:00 +0900

    [chore] cypress path alias 적용

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,21 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: async (config) => {
+    // https://storybook.js.org/docs/react/builders/webpack#typescript-module-resolution
+    // Apply storybook path alias
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    ];
+    return config;
   },
 };
