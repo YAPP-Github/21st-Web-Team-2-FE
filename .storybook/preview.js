27e5063afd1973977628733c8e7d19edee9db3cc
hwookim <gusdn9754@gmail.com>
2022-12-04 05:16:59 +0900

    chore: storybook 설정 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,6 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { initialize, mswDecorator } from 'msw-storybook-addon';
+import { RouterContext } from 'next/dist/shared/lib/router-context';
 import * as NextImage from 'next/image';
 
 import mockApis from '../__mocks__/apis';
@@ -6,11 +8,21 @@ import '../src/styles/reset.css';
 
 initialize();
 
-export const decorators = [mswDecorator];
+export const decorators = [
+  mswDecorator,
+  (Story) => (
+    <QueryClientProvider client={new QueryClient()}>
+      <Story />
+    </QueryClientProvider>
+  ),
+];
 
 export const parameters = {
   msw: mockApis,
   actions: { argTypesRegex: '^on.*' },
+  nextRouter: {
+    Provider: RouterContext.Provider,
+  },
 };
 
 const OriginalNextImage = NextImage.default;
