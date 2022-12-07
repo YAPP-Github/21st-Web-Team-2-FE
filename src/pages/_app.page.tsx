b7595e000381bc6b9d28fabf177bfd23e99d4520
sujk912 <aa991122aa@gmail.com>
2022-12-07 23:26:23 +0900

    refactor: Remove Example from _app.page.tsx

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -3,14 +3,12 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 
-import Example from '../components/Example';
 import queryClient from '../configs/queryClient';
 import '../styles/reset.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <Example title={'TEST'} />
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
         <ReactQueryDevtools initialIsOpen={false} />
