11bc8a1eeecc305a749ef286a27a30b9585e64f7
sujk912 <aa991122aa@gmail.com>
2023-01-01 20:24:08 +0900

    절대 경로로 이동후 app.page.tsx로 이동

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -2,10 +2,24 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
+import 'src/styles/common.css';
+import 'src/styles/reset.css';
 
 import queryClient from '../configs/queryClient';
-import '../styles/common.css';
-import '../styles/reset.css';
+
+if (process.env.NODE_ENV === 'development') {
+  if (typeof window === 'undefined') {
+    (async () => {
+      const { server } = await import('@mocks/apis/server');
+      server.listen();
+    })();
+  } else {
+    (async () => {
+      const { worker } = await import('@mocks/apis/browser');
+      await worker.start();
+    })();
+  }
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
