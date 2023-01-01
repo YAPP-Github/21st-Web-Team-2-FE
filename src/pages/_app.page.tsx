b501ae97555b5c9af1dcf64165645b39222a7589
sujk912 <aa991122aa@gmail.com>
2023-01-01 23:22:58 +0900

    reset이 common보다 먼저오도록 수정

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -2,8 +2,8 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
-import 'src/styles/common.css';
 import 'src/styles/reset.css';
+import 'src/styles/common.css';
 
 import queryClient from '../configs/queryClient';
 
