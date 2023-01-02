04427a7b8afdedadd4a197e8175bfbf2b086b424
kimseunggyu <dev.seunggyu@gmail.com>
2023-01-02 17:18:18 +0900

    [feat] Nextjs 에서 svgr 파일 적용되도록 설정 - @svgr/webpack 는 SVG 파일을 인식할 수 있도록 설정 - url-loader 은 SVG 파일을 ReactComponent 로 인식할 수 있게 하도록 설정

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const nextConfig = {
   swcMinify: true,
   // 🐛 pages 파일에 test 파일있으면 issue 있어서, page 파일명을 변경하여 수정
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack', 'url-loader'],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
