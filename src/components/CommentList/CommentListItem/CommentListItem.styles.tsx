90d57b332239ab7316a1f3b1eb0c8c9a3eba9d54
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2023-01-07 15:42:54 +0900

    [feat] CommentItem 댓글에 좋아요 누른 경우 스타일 변경

--- src/components/CommentList/CommentListItem/CommentListItem.styles.tsx
+++ src/components/CommentList/CommentListItem/CommentListItem.styles.tsx
@@ -36,15 +36,22 @@ export const UserInfo = styled.p`
 
 export const Comment = styled.p``;
 
-export const VoteIcon = styled(Icon)`
-  &:hover {
-    cursor: pointer;
-  }
-`;
-
-export const VoteWrapper = styled.div`
+export const VoteWrapper = styled.div<{ $liked: boolean }>`
   display: flex;
   align-items: center;
   font-size: 12px;
   line-height: 18px;
+
+  ${({ $liked }) =>
+    $liked &&
+    `color: ${theme.color.Primary1};
+      & path {
+      fill: ${theme.color.Primary1};
+    }`}
+`;
+
+export const VoteIcon = styled(Icon)`
+  &:hover {
+    cursor: pointer;
+  }
 `;
