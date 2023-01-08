44f4cc651dd17ef4e8883a568e41664a47a94bc3
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2023-01-08 12:00:58 +0900

    [feat] 토픽 추가 fab 버튼 UI 구현

---
+++ src/components/addButton/FabButton/FabButton.styles.tsx
@@ -0,0 +1,27 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+import theme from '@src/styles/theme';
+
+export const FabButton = styled.button<{ $visible: boolean }>`
+  visibility: hidden;
+  cursor: pointer;
+  width: 58px;
+  height: 58px;
+  border-radius: 50%;
+  background: ${theme.color.Primary1};
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  position: absolute;
+  right: 28px;
+  bottom: 28px;
+
+  ${({ $visible }) =>
+    $visible &&
+    css`
+      visibility: visible;
+    `}
+`;
