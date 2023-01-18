4b12a79517934a6a8c0e99b6d1cb91efce324791
hwookim <gusdn9754@gmail.com>
2023-01-18 17:29:02 +0900

    feat: Main 컴포넌트 구현

---
+++ src/components/main/Main/Main.styles.tsx
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled';
+
+export const Observer = styled.div`
+  margin-bottom: 80px;
+  height: 20px;
+`;
--- src/components/main/MainTopicList/MainTopicList.tsx
+++ src/components/main/MainTopicList/MainTopicList.tsx
@@ -1,9 +1,7 @@
 import { useRouter } from 'next/router';
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React from 'react';
 
 import TopicCard from '@src/components/common/TopicCard';
-import AddButton from '@src/components/main/AddButton';
-import FabButton from '@src/components/main/FabButton';
 import Member from '@src/types/Member';
 import Topic from '@src/types/Topic';
 
@@ -15,42 +13,15 @@ export interface MainTopicListProps {
 }
 
 const MainTopicList: React.FC<MainTopicListProps> = (props) => {
-  const { member, topics } = props;
-  const [fabVisible, setFabVisible] = useState<boolean>(false);
-  const observerRef = useRef<HTMLDivElement>(null);
+  const { topics } = props;
   const router = useRouter();
 
-  const handleObserver: IntersectionObserverCallback = useCallback(
-    ([target]: IntersectionObserverEntry[]) => setFabVisible(!target.isIntersecting),
-    [],
-  );
-
-  useEffect(() => {
-    if (!observerRef.current) return;
-    const observer = new IntersectionObserver(handleObserver);
-
-    observer.observe(observerRef.current);
-  }, [handleObserver]);
-
-  const handleWrite = () => {
-    router.push('/write');
-  };
-
   return (
-    <>
-      <S.MainTop>
-        <S.Welcome>Hello {member?.nickname || 'Fingers'}!</S.Welcome>
-        <div ref={observerRef} />
-        <S.SubText>{member?.nickname || '당신'}의 선택은 무엇인가요?</S.SubText>
-      </S.MainTop>
-      <AddButton onClick={handleWrite} />
-      <S.TopicsContainer>
-        {topics.map((topic) => (
-          <TopicCard key={topic.id} {...topic} type="feed" badge="참여율 TOP" onClick={() => router.push('/topic/1')} />
-        ))}
-      </S.TopicsContainer>
-      <FabButton onClick={handleWrite} visible={fabVisible} />
-    </>
+    <S.TopicsContainer>
+      {topics.map((topic) => (
+        <TopicCard key={topic.id} {...topic} type="feed" onClick={() => router.push('/topic/1')} />
+      ))}
+    </S.TopicsContainer>
   );
 };
 
