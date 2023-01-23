db747dbe6d76174fe423901ff0695cd4c479d2d1
hwookim <gusdn9754@gmail.com>
2023-01-23 17:16:43 +0900

    feat: 환경변수를 통한 api 주소 호출

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// TODO-GYU: 배포시 실제 API 로 변경되는 로직 추가
-export const BASE_URL = 'http://localhost:3000/api/v1';
+export const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const initAxiosConfig = () => {
   axios.defaults.baseURL = BASE_URL ?? '';
