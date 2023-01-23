e0ea19f8ea6cd0c61ad970f23b461a27fff05e96
hwookim <gusdn9754@gmail.com>
2023-01-23 20:43:06 +0900

    feat: ShareIcon 구현

---
+++ src/components/common/ShareIcon/ShareIcon.styles.tsx
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+
+import Icon from '@src/components/common/Icon';
+import theme from '@src/styles/theme';
+
+export const Container = styled.div`
+  width: 24px;
+
+  position: relative;
+`;
+
+export const Share = styled(Icon)`
+  cursor: pointer;
+`;
+
+export const Toast = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 2px;
+
+  position: absolute;
+  width: 200px;
+  height: 48px;
+  top: calc(100% + 8px);
+  right: 0;
+
+  background-color: ${theme.color.G6};
+  border-radius: 8px;
+
+  font-weight: ${theme.fontWeight.bold};
+  font-size: ${theme.textSize.T3};
+  line-height: ${theme.lineHeight.B};
+`;
---
+++ src/components/common/ShareIcon/ShareIcon.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+import Icon from '@src/components/common/Icon';
+
+import * as S from './ShareIcon.styles';
+
+interface ShareIconProps {
+  url: string;
+}
+
+const ShareIcon: React.FC<ShareIconProps> = (props) => {
+  const { url } = props;
+  const [isCopied, setIsCopied] = useState<boolean>(false);
+
+  const handleShare = async () => {
+    if (isCopied) return;
+    if (!navigator.clipboard) return;
+
+    await navigator.clipboard.writeText(url);
+    setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
+  };
+
+  return (
+    <S.Container>
+      <S.Share name="Share" onClick={handleShare} />
+      {isCopied && (
+        <S.Toast>
+          <Icon name="Smile" />
+          링크가 복사되었습니다!
+        </S.Toast>
+      )}
+    </S.Container>
+  );
+};
+
+export default ShareIcon;
