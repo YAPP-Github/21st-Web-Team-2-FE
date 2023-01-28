b5d02e8d9e25bb308b9be3c9ee68d0e39fb039a7
hwookim <gusdn9754@gmail.com>
2023-01-29 01:53:06 +0900

    refactor: codeblock mockdata화

--- __mocks__/data/voteOption.ts
+++ __mocks__/data/voteOption.ts
@@ -26,58 +26,60 @@ export const VOTE_OPTION_WITH_IMAGE: VoteOption = {
   image: TEST_IMAGE,
 };
 
+export const CODE_BLOCK: VoteOption['codeBlock'] = {
+  language: 'js',
+  contents: `
+  import React, { MouseEvent } from 'react';
+  
+  import { Languages } from '@src/components/common/CodeEditor';
+  import Icon from '@src/components/common/Icon';
+  import VoteOption from '@src/types/VoteOption';
+  
+  import * as S from './SelectOption.style';
+  
+  interface SelectOptionProps extends VoteOption {
+    rate?: number;
+    result?: boolean;
+    selected?: boolean;
+    onClick: (id: number, selected: boolean) => void;
+  }
+  
+  const SelectOption = (props: SelectOptionProps) => {
+    const { voteOptionId, text, rate = 0, result = false, selected = false, image, codeBlock, onClick } = props;
+  
+    const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+      e.stopPropagation();
+      e.nativeEvent.preventDefault();
+      onClick(voteOptionId, selected);
+    };
+  
+    return (
+      <S.Container>
+        <S.SelectButton $result={result} $selected={selected} onClick={handleClick}>
+          {result && <S.ProgressBar className="progress" $rate={rate} $selected={selected} />}
+          <S.Info>
+            <Icon name="Vote" />
+            <S.Text>{text}</S.Text>
+            {result && <S.Rate>{Math.round(rate * 100)}%</S.Rate>}
+          </S.Info>
+        </S.SelectButton>
+        {image && (
+          <S.ImageWrapper>
+            <S.OptionImage src={image} alt={text} fill />
+          </S.ImageWrapper>
+        )}
+        {codeBlock && <S.CodeBlock language={codeBlock.language as Languages} value={codeBlock.contents} disabled />}
+      </S.Container>
+    );
+  };
+  
+  export default SelectOption;
+      `,
+};
+
 export const VOTE_OPTION_WITH_CODE: VoteOption = {
   ...VOTE_OPTION,
   voteOptionId: 9,
   text: '코드가 있어요!',
-  codeBlock: {
-    language: 'js',
-    contents: `
-import React, { MouseEvent } from 'react';
-
-import { Languages } from '@src/components/common/CodeEditor';
-import Icon from '@src/components/common/Icon';
-import VoteOption from '@src/types/VoteOption';
-
-import * as S from './SelectOption.style';
-
-interface SelectOptionProps extends VoteOption {
-  rate?: number;
-  result?: boolean;
-  selected?: boolean;
-  onClick: (id: number, selected: boolean) => void;
-}
-
-const SelectOption = (props: SelectOptionProps) => {
-  const { voteOptionId, text, rate = 0, result = false, selected = false, image, codeBlock, onClick } = props;
-
-  const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
-    e.stopPropagation();
-    e.nativeEvent.preventDefault();
-    onClick(voteOptionId, selected);
-  };
-
-  return (
-    <S.Container>
-      <S.SelectButton $result={result} $selected={selected} onClick={handleClick}>
-        {result && <S.ProgressBar className="progress" $rate={rate} $selected={selected} />}
-        <S.Info>
-          <Icon name="Vote" />
-          <S.Text>{text}</S.Text>
-          {result && <S.Rate>{Math.round(rate * 100)}%</S.Rate>}
-        </S.Info>
-      </S.SelectButton>
-      {image && (
-        <S.ImageWrapper>
-          <S.OptionImage src={image} alt={text} fill />
-        </S.ImageWrapper>
-      )}
-      {codeBlock && <S.CodeBlock language={codeBlock.language as Languages} value={codeBlock.contents} disabled />}
-    </S.Container>
-  );
-};
-
-export default SelectOption;
-    `,
-  },
+  codeBlock: CODE_BLOCK,
 };
