35bb75d44ccfd0f40740367641e4530e35c4abb6
kimseunggyu <dev.seunggyu@gmail.com>
2023-02-04 15:23:25 +0900

    [refactor] ContentJob 이름 변경

--- src/components/onboarding/ContentJob/ContentJob.stories.tsx
+++ src/components/onboarding/ContentJob/ContentJob.stories.tsx
@@ -1,14 +1,14 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
-import ContentName from './ContentJob';
+import ContentJob from './ContentJob';
 
 export default {
   title: 'onboarding/ContentJob',
-  component: ContentName,
+  component: ContentJob,
   args: {},
-} as ComponentMeta<typeof ContentName>;
+} as ComponentMeta<typeof ContentJob>;
 
-const Template: ComponentStory<typeof ContentName> = ({ ...args }) => <ContentName {...args} />;
+const Template: ComponentStory<typeof ContentJob> = ({ ...args }) => <ContentJob {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {};
