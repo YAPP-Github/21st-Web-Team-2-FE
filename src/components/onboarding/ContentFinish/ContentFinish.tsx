a268849b4d798b41255fe64af62a25049d8d12fd
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2023-02-05 00:38:25 +0900

    [feat] 직업 선택 기능 구현

--- src/components/onboarding/ContentFinish/ContentFinish.tsx
+++ src/components/onboarding/ContentFinish/ContentFinish.tsx
@@ -4,17 +4,17 @@ import { Onboarding } from '@src/pages/onboarding';
 
 import * as S from './ContentFinish.styles';
 
-// GYU-TODO: 설정한 닉네임 설정해야함!
 interface Props {
   onboardingValue: Onboarding;
 }
 const ContentFinish: FC<Props> = (props) => {
   const { onboardingValue } = props;
 
+  const job = onboardingValue.job as string;
   return (
     <S.Wrapper>
-      <S.JobIcon name={getJobName('developer')} size={80} />
-      <S.Title>Hello Typing!</S.Title>
+      <S.JobIcon name={getJobName(job) ?? 'Developer'} size={80} />
+      <S.Title>Hello {onboardingValue.name}!</S.Title>
       <S.Title>We are Thumbs UP</S.Title>
       <S.SubTitle>궁금한 토픽을 만들고 같이 이야기 나눠봐요!</S.SubTitle>
       <S.Button>시작하기</S.Button>
@@ -22,14 +22,15 @@ const ContentFinish: FC<Props> = (props) => {
   );
 };
 
-const getJobName = (job: 'designer' | 'developer' | 'pm') => {
-  const jobObj = {
+// GYU-TODO: any 타입 수정하기
+const getJobName = (job: 'designer' | 'developer' | 'pm' | string) => {
+  const jobObj: any = {
     designer: 'Designer',
     developer: 'Developer',
     pm: 'Pm',
   } as const;
 
-  return jobObj[job];
+  return jobObj[job] ?? 'Developer';
 };
 
 export default ContentFinish;
--- src/components/onboarding/ContentJob/ContentJob.tsx
+++ src/components/onboarding/ContentJob/ContentJob.tsx
@@ -2,28 +2,32 @@ import React, { ChangeEvent, FC, useState } from 'react';
 
 import Icon from '@src/components/common/Icon';
 import Input from '@src/components/common/Input';
-import { Onboarding } from '@src/pages/onboarding';
+import { Onboarding, OnboardingKey } from '@src/pages/onboarding';
 
 import * as S from './ContentJob.styles';
 
+type Job = 'developer' | 'designer' | 'pm' | 'etc' | null;
 interface Props {
   onboardingValue: Onboarding;
-  onChangeOnboardingStep: (key: 'name' | 'job' | 'year', value: string) => void;
+  onChangeOnboardingStep: (key: OnboardingKey, value: string) => void;
 }
 // GYU-TODO: 입력시 다음 버튼 및 직업 설정 기능 구현
 const ContentName: FC<Props> = (props) => {
-  const [job, setJob] = useState('');
+  const [job, setJob] = useState<Job>(null);
   const [etcValue, setEtcValue] = useState('');
-
-  const handleClickJob = (_job: string) => {
-    setJob(_job);
-  };
 
   const handleChangeEtc = (e: ChangeEvent<HTMLInputElement>) => {
     setEtcValue(e.target.value);
   };
 
   const handleClickNext = () => {
+    if (job === null) return;
+
+    if (job === 'etc') {
+      onChangeOnboardingStep('job', etcValue);
+      return;
+    }
+
     onChangeOnboardingStep('job', job);
   };
 
@@ -36,32 +40,39 @@ const ContentName: FC<Props> = (props) => {
       <S.SubTitle>반가워요, {name}님 : )</S.SubTitle>
       <S.SubTitle>현재 어떤 일을 하고 계신가요?</S.SubTitle>
       <S.ButtonWrapper>
-        <S.SelectButton onClick={() => handleClickJob('developer')} $selected={job === 'developer'}>
+        <S.SelectButton onClick={() => setJob('developer')} $selected={job === 'developer'}>
           <Icon name="Computer" size={18} />
           개발
         </S.SelectButton>
-        <S.SelectButton onClick={() => handleClickJob('designer')} $selected={job === 'designer'}>
+        <S.SelectButton onClick={() => setJob('designer')} $selected={job === 'designer'}>
           <Icon name="Paint" size={18} />
           디자인
         </S.SelectButton>
-        <S.SelectButton onClick={() => handleClickJob('pm')} $selected={job === 'pm'}>
+        <S.SelectButton onClick={() => setJob('pm')} $selected={job === 'pm'}>
           <Icon name="Paper" size={18} />
           기획
         </S.SelectButton>
       </S.ButtonWrapper>
       <S.EtcWrapper>
-        <S.SelectButton onClick={() => handleClickJob('etc')} $selected={job === 'etc'}>
+        <S.SelectButton onClick={() => setJob('etc')} $selected={job === 'etc'}>
           기타
         </S.SelectButton>
         {job === 'etc' && <Input value={etcValue} onChange={handleChangeEtc} style={{ height: '40px' }} />}
       </S.EtcWrapper>
       <S.ButtonWrapper>
-        <S.Button disabled={!job} onClick={handleClickNext}>
+        <S.Button disabled={checkDisabled(job, etcValue)} onClick={handleClickNext}>
           다음
         </S.Button>
       </S.ButtonWrapper>
     </S.Wrapper>
   );
+};
+
+const checkDisabled = (job: Job, etc: string) => {
+  if (job === null) return true;
+  if (job === 'etc' && etc.trim() === '') true;
+
+  return false;
 };
 
 export default ContentName;
