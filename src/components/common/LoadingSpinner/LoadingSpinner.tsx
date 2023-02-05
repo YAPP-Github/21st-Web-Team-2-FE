0e35750aab208709b88a53410d092d10a02abd49
kimseunggyu <dev.seunggyu@gmail.com>
2023-02-05 21:48:53 +0900

    [feat] LoadingSpinner UI 구현

---
+++ src/components/common/LoadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react';
+
+import theme from '@src/styles/theme';
+
+import * as S from './LoadingSpinner.styles';
+
+interface Props {
+  /**
+   * hex값 string 또는 color string(red, blue, ...)
+   */
+  color: string;
+  size: number;
+}
+const LoadingSpinner: FC<Props> = (props) => {
+  const { color = theme.color.Primary1, size = 50 } = props;
+
+  return (
+    <S.Background>
+      <S.Spinner role="progressbar" $size={size} $color={color} />
+    </S.Background>
+  );
+};
+
+export default LoadingSpinner;
