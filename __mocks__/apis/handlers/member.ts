16bab3bd7d4cade79111bb2af8c8eb945ed94c8b
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2023-02-11 03:13:18 +0900

    [feat] member authorization 검증 코드 추가

--- __mocks__/apis/handlers/member.ts
+++ __mocks__/apis/handlers/member.ts
@@ -2,16 +2,28 @@ import { ResponseComposition, RestContext, RestRequest, rest } from 'msw';
 
 import { MEMBER } from '@mocks/data/member';
 
-import { GetMemberResponse } from '@src/apis';
+import { ErrorResponse, GetMemberResponse } from '@src/apis';
 import { BASE_URL } from '@src/configs/axios';
 
 const getMember = (req: RestRequest, res: ResponseComposition, ctx: RestContext) => {
+  const authorization = req.headers.get('authorization');
+
+  if (authorization) {
+    return res(
+      ctx.status(200),
+      ctx.json<GetMemberResponse>({
+        code: 'SUCCESS',
+        message: '성공',
+        data: MEMBER,
+      }),
+    );
+  }
+
   return res(
-    ctx.status(200),
-    ctx.json<GetMemberResponse>({
-      code: 'SUCCESS',
-      message: '성공',
-      data: MEMBER,
+    ctx.status(401),
+    ctx.json<ErrorResponse>({
+      code: '2003',
+      message: '엑세스 토큰이 필요합니다.',
     }),
   );
 };
