ce1a6ac1f1d75baa3dc27429d16f21761abe7b01
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2023-02-11 11:29:56 +0900

    [fix] 🐛 api 요청시 Main Page 에러가 발생하여 임시로 해당 부분 수정 - 임시이기 때문에 완전한 문제 해결 필요 - TopicCard 에 topic 데이터가 없는 경우 에러가 발생하여 시작점에 topics 데이터가 있어야 렌더링 되게 수정

--- src/components/main/Main/Main.tsx
+++ src/components/main/Main/Main.tsx
@@ -37,7 +37,12 @@ const Main: React.FC<MainProps> = (props) => {
 
   return (
     <>
-      <TopicCarousel topics={popularTopics} />
+      {/*GYU-TODO:  해당 부분에서 빈배열로 데이터가 오는 경우 처리가 안되어 있어서 임시로 처리
+          @현우님이 해당 부분 참고하셔서 수정하시면 될거 같아요! 
+          TopicCard 에서 topic 이 없을때 에러가 발생합니다!
+          시작점이 이곳이라서 이곳에 임시 에러 방지 구현했습니다.
+      */}
+      {popularTopics.length ? <TopicCarousel topics={popularTopics} /> : null}
       <S.Observer ref={observerRef} />
       <AddButton onClick={handleWrite} />
       <MainTopicList />
