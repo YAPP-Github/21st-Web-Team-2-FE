ad9a2deb988443860f312e396dd483f41adbca27
hwookim <gusdn9754@gmail.com>
2023-02-14 15:50:31 +0900

    feat: 글쓰기 페이지 접근 시 로그인 검사

--- src/pages/write/index.page.tsx
+++ src/pages/write/index.page.tsx
@@ -1,11 +1,23 @@
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
 
 import Header from '@src/components/common/Header';
 import WriteMain from '@src/components/write/WriteMain';
 import useCreateTopic from '@src/queires/useCreateTopic';
+import $userSession from '@src/recoil/userSession';
 
 const WritePage: NextPage = () => {
   const { createTopic } = useCreateTopic();
+  const tokens = useRecoilValue($userSession);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (tokens) return;
+    alert('로그인이 필요합니다!');
+    router.push('/login');
+  }, [router, tokens]);
 
   return (
     <>
