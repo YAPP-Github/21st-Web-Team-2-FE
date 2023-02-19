0545c5de9e55e51c49e3147b086ab4a198d03d62
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2023-02-19 18:45:02 +0900

    [refactor] 오타 수정

--- src/components/common/SEO/SEO.tsx
+++ src/components/common/SEO/SEO.tsx
@@ -1,12 +1,12 @@
 import { NextSeo } from 'next-seo';
 import { FC } from 'react';
 
-interface Prosp {
+interface Props {
   title: string;
   description: string;
   image?: string;
 }
-const SEO: FC<Prosp> = ({ title, description, image }) => {
+const SEO: FC<Props> = ({ title, description, image }) => {
   return (
     <NextSeo
       title={title}
