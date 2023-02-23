fc3a314ee03d4f5a799d5ce84b04da06ebf10343
hwookim <gusdn9754@gmail.com>
2023-02-24 02:24:55 +0900

    feat: VoteStatistics 구현

---
+++ src/components/common/TopicCard/VoteStatistics/VoteStatistics.style.tsx
@@ -0,0 +1,33 @@
+import styled from '@emotion/styled';
+
+import theme from '@src/styles/theme';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2px;
+`;
+
+export const Main = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 2px;
+
+  ${theme.font.B3}
+  color: ${theme.color.G8};
+`;
+
+export const Sub = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 6px;
+
+  ${theme.font.B4}
+  color: ${theme.color.G7};
+`;
+
+export const SubItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 2px;
+`;
---
+++ src/components/common/TopicCard/VoteStatistics/index.tsx
@@ -0,0 +1,2 @@
+export { default } from './VoteStatistics';
+export * from './VoteStatistics';
