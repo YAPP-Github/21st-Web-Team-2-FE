7e622b3a24a788ef8dc4919ce48834d1a08620c6
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2023-02-25 19:30:59 +0900

    [fix] 임시로 open graph 적용하기

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -5,6 +5,12 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta name="description" content="www.thumbs-up.me"></meta>
+          <meta name="twitter:card" content="summary_large_image"></meta>
+          <meta property="og:title" content="IT 직무 관련자들을 위한 투표 커뮤니티 웹 플랫폼 | 텀즈업"></meta>
+          <meta property="og:type" content="website"></meta>
+          <meta property="og:description" content="www.thumbs-up.me"></meta>
+          <meta property="og:image" content="https://www.thumbs-up.me/og.png"></meta>
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <body>
